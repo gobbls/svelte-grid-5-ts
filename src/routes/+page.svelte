808291d50365a78b5020c72184a0876f6199af0b
gobbls <98641699+gobbls@users.noreply.github.com>
2026-05-10 17:22:50 +0200

    Replaced strings for `...?raw` import for sample snippets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,101 +1,19 @@
 <script lang="ts">
 import { HighlightSvelte, Highlight } from 'svelte-highlight';
 import typescript from 'svelte-highlight/languages/typescript';
-import GettingStartedExampleGrid from './GettingStartedExampleGrid.svelte';
+
 import Demo from './Demo.svelte';
+import GettingStartedExampleGrid from './GettingStartedExampleGrid.svelte';
 
-const codeImports: string =
-	`<script lang="ts">
-  import Grid from 'svelte-grid-5-ts';
-  import type { Item, Size } from 'svelte-grid-5-ts/types/item';
-  import type { OnChange } from 'svelte-grid-5-ts/types/funcs';
-</scr` + 'ipt>';
+import SpecString from './_snippets/Spec.svelte?raw';
+import ImportsString from './_snippets/Imports.svelte?raw';
+import DivWrapperString from './_snippets/DivWrapper.svelte?raw';
+import BindItemsString from './_snippets/BindItems.svelte?raw';
+import EventPropString from './_snippets/EventProp.svelte?raw';
+import EventHandlerString from './_snippets/EventHandler.svelte?raw';
+import StyleString from './_snippets/Style.svelte?raw';
 
-const codeDivWrapper: string = '<div class="sg5-wrapper"></div>';
-
-const codeSpec: string =
-	`<script lang="ts">
-  /* ... */
-  const COL = 10;             // The amount of columns for the grid at the current breakpoint.
-  const cols = [[1200, COL]]; // Breakpoints and columns.
-
-  /*
-   * Here is a quick summary for each property.
-   * Check out the "Usage" page for detailed information.
-   */
-
-  // The collection of items to bind. These are dynamically updated when moved or resized.
-  // Think of an "item" as a "frame" or "container" for your widget's content.
-  let items: Item[] = $state([
-    {
-      id: 'move me around',   // Has to be unique for each item (a UUID for example).
-      data: '',               // The data you want to pass to your widget.
-      [COL]: {                // 'COL' is used to match the grid column size.
-      	fixed: false,         // Cannot be moved or resized.
-      	resizable: true,      // If the item can be resized.
-      	draggable: true,      // If the item can be dragged around.
-      	customDragger: false, // If you want to use a custom dragger, maybe an icon?
-      	customResizer: false, // If you want to use a custom resizer.
-      	min: { w: 1, h: 1 },  // The minimum size of the item.
-      	max: {} as Size,      // The maximum size of the item.
-      	x: 0,                 // Distance from the containers left border.
-      	y: 0,                 // Distance from the containers top border.
-      	w: 2,                 // The width, in columns, of the item.
-      	h: 3                  // The height, in rows, of the item.
-      }
-    }
-  ]);
-</scr` + 'ipt>';
-
-const codeBindItems: string = `<div class="sg5-wrapper">
-  <Grid
-    bind:items={items}
-    cols={cols}
-    gap={[2, 2]}
-    rowHeight={50}
-    fastStart={true}
-  >
-  </Grid>
-</div>`;
-
-const codeEventProp: string = `<div class="sg5-wrapper">
-  <Grid
-    /* ... */
-    onChange={handleOnChange}
-  >
-    {#snippet children({ dataItem }: { dataItem: Item })}
-      <div class="sg5-item">{dataItem.id}</div>
-    {/snippet}
-  </Grid>
-</div>`;
-
-const codeEventHandler: string = `/* ... */
-let updates: number = $state(0);
-
-function handleOnChange({ ...data }: OnChange): void {
-  updates++;
-  console.log(\`[UPDATE: \${updates}] Have a look at the data you can use:\`, data);
-};
-/* ... */`;
-
-const codeStyle: string =
-	`<style>
-  .sg5-wrapper {
-    max-width: 760px;
-    box-sizing: border-box;
-    border: 1px solid white;
-    background-color: #161618;
-    margin-top: 30px;
-  }
-
-  .sg5-item {
-    width: 100%;
-    height: 100%;
-    color: #161618;
-    background-color: #a8b1ff;
-    overflow: hidden;
-  }
-</sty` + 'le>';
+const untab = (svlt: string): string => svlt.replaceAll('\t', '    ');
 </script>
 
 <svelte:head>
@@ -167,22 +85,24 @@ const codeStyle: string =
 
 	<li>
 		Start by importing the main <code>Grid</code> component and the <code>Item, Size</code> types, as well as the
-		<code>OnChange</code> type, as it will be used in this example.<HighlightSvelte code={codeImports} />
+		<code>OnChange</code> type, as it will be used in this example.
+		<HighlightSvelte code={ImportsString} />
 	</li>
 
 	<li>
 		Create a<code>div</code> wrapper for the <code>Grid</code> component, it will be handy for listening to its size and
-		target for styling. <HighlightSvelte code={codeDivWrapper} />
+		target for styling.
+		<HighlightSvelte code={DivWrapperString} />
 	</li>
 
 	<li>
-		Specify the specs for the grid, then create an array with a single item. <HighlightSvelte code={codeSpec} />
+		Specify the specs for the grid, then create an array with a single item.
+		<HighlightSvelte code={untab(SpecString)} />
 	</li>
 
 	<li>
-		Place the <code>Grid</code> component in the wrapper, bind the items and pass the specs to the component as props. <HighlightSvelte
-			code={codeBindItems}
-		/>
+		Place the <code>Grid</code> component in the wrapper, bind the items and pass the specs to the component as props.
+		<HighlightSvelte code={untab(BindItemsString)} />
 	</li>
 
 	<li>
@@ -190,19 +110,25 @@ const codeStyle: string =
 		a widget &mdash; a <code>div</code> here, for the sake of example &mdash; as a snippet for the item to render. The
 		<code>children</code>
 		snippet returns multiple properties for us to use. Here, we use the <code>dataItem</code> property to receive the ID
-		of our item to be used in our <code>div</code>. <HighlightSvelte code={codeEventProp} />
+		of our item to be used in our <code>div</code>.
+		<HighlightSvelte code={untab(EventPropString)} />
 	</li>
 
 	<li>
-		Create a function to fire when the event occurs. <Highlight language={typescript} code={codeEventHandler} />
+		Create a function to fire when the event occurs.
+		<Highlight language={typescript} code={untab(EventHandlerString)} />
 	</li>
 
 	<li>
-		To finish off this example, add some styling to the wrapper and your widget. <HighlightSvelte code={codeStyle} /> Now,
-		with all the steps applied, your example should look like this.
+		To finish off this example, add some styling to the wrapper and your widget.
+		<HighlightSvelte code={untab(StyleString)} />
+
+		Now, with all the steps applied, your example should look like this.
+
 		<blockquote>
 			Open your browser console and move the item around to see the <code>OnChange</code> function run.
 		</blockquote>
+
 		<GettingStartedExampleGrid />
 	</li>
 </ol>
